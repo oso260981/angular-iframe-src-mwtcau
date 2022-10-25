27c653caa97bd902a4f3aa62fe4f87652c94112f
oso260981 <71036844+oso260981@users.noreply.github.com>
2022-10-25 10:47:38 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,16 +3,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Set iframe source';
-  url: string = "https://app.powerbi.com/reportEmbed?reportId=77f3e5a8-b465-4f55-8bc8-61512086dad5&autoAuth=true&ctid=2b60f742-477e-42f8-a7ad-3621d21bc607";
+  url: string = 'https://looker.isrstoreintelligence.online/taxtools.html';
   urlSafe: SafeResourceUrl;
 
-  constructor(public sanitizer: DomSanitizer) { }
+  constructor(public sanitizer: DomSanitizer) {}
 
   ngOnInit() {
-    this.urlSafe= this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+    this.urlSafe = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
   }
 }
